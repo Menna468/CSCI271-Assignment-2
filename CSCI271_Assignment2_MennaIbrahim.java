41eef5536e8317e134449c1abe2beb2a1b931ef2
Menna Ibrahim <ibrahimm2@winthrop.edu>
2026-02-06 09:53:51 -0500

    done

--- CSCI271_Assignment2_MennaIbrahim.java
+++ CSCI271_Assignment2_MennaIbrahim.java
@@ -40,15 +40,6 @@ public class CSCI271_Assignment2_MennaIbrahim{
         Fraction results = c.multiply(a.divide(b));
         System.out.println(results);
 
-        /*---------------------------Test Case 1---------------------------*/
-        //uses multiply and negate
-        Fraction d = new Fraction(16);
-        Fraction e = new Fraction(2);
-        Fraction results2 = d.multiply(e).negate();
-        Fraction results2a = d.divide(e).negate();
-        System.out.println(results2);
-        System.out.println(results2a);
-
 
         /*---------------------------Test Cases 3---------------------------*/
         //use negate and divide
@@ -65,39 +56,49 @@ public class CSCI271_Assignment2_MennaIbrahim{
         Fraction a6 = a5.negate();
         System.out.println(a6);
 
+        /*---------------------------Test Case 1---------------------------*/
+        //uses multiply and negate
+        Fraction d = new Fraction(16);
+        Fraction e = new Fraction(2);
+        Fraction results2 = d.multiply(e).negate();
+        Fraction results2a = d.divide(e).negate();
+        System.out.println(results2);
+        System.out.println(results2a);
+
         /*---------------------------Test Case 4---------------------------*/
         //uses subtract
-        Fraction b1 = new Fraction(2,3);
-        Fraction b2 = new Fraction(4,5);
-        Fraction result3 = b1.subtract(b2);
-        System.out.println(result3);
+        Fraction f = new Fraction(2,3).subtract(new Fraction(4,5));
+        System.out.println(f);
 
         /*---------------------------Test Case 5---------------------------*/
         //uses divide and negate
-        Fraction b3 = new Fraction(2,3);
-        Fraction b4 = new Fraction(0);
-        Fraction result4 = b3.divide(b4);
-        Fraction result5 = result4.negate();
-        System.out.println(result4);
-        System.out.println(result5);
+        Fraction g = new Fraction(2,3).divide(new Fraction(0)).negate();
+        System.out.println(g);
 
-        Fraction result6 = result5.negate();
-        System.out.println(result6);
+        Fraction h = g.negate();
+        System.out.println(h);
 
 
         /*---------------------------Test Case 6---------------------------*/
         //uses pow
-        Fraction c1 = new Fraction(2);
-        Fraction result7 = c1.pow(3);
-        System.out.println(result7);
+        Fraction i = new Fraction(2).pow(3);
+        System.out.println(i);
 
 
         /*---------------------------Test Case 7---------------------------*/
         //uses add
-        Fraction d1 = new Fraction(6,15);
-        Fraction d2 = new Fraction(9,15);
-        Fraction result8 = d1.add(d2);
-        System.out.println(result8);
+        Fraction j = new Fraction(6,15).add(new Fraction(9,15));
+        System.out.println(j);
+
         
+        /*---------------------------Test Case 8---------------------------*/
+        //uses pow
+        Fraction k = new Fraction(1,2).pow(-2);
+        System.out.println(k);
+
+        /*---------------------------Test Case 8---------------------------*/
+        //uses pow
+        Fraction l = new Fraction(50).pow(0);
+        System.out.println(l);
     }
 }
